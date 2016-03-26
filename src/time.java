1557f605afca9d303b466b2454b308bfd094e130
Adauto Braz da Silva Neto <adautobraz.neto@gmail.com>
2016-03-26 19:39:19 -0300

    Encapsulamento terminado

--- src/time.java
+++ src/time.java
@@ -1,14 +1,22 @@
 
 public class time {
-	int hour;
-	int minute;
+	private int hour;
+	private int minute;
 	
 	time(){};
 	
+	public void setTime(int aHour, int aMinute){
+		this.hour = aHour;
+		this.minute = aMinute;
+	}
+	
+	public String getTime(){
+		return hour+":"+minute;
+	}
+	
 	public static void main(String arg[]){
 		time t = new time();
-		t.hour = 3;
-		t.minute = 25;
-		System.out.println("The time now is" + t.hour + ":" + t.minute);	
+		t.setTime(3, 25);
+		System.out.println("The time now is" + t.getTime());		
 	}
 }
